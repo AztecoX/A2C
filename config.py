6497eee0a3c3721c212ebd92511e227f3bec6d83
Michael Vlcek <xvlcek21@pcfajcik.fit.vutbr.cz>
2018-03-17 11:50:15 +0100

    Ignoring config file

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ class Config:
         flags.DEFINE_bool("visualize", False, "Whether to render with pygame.")
         flags.DEFINE_integer("resolution", 32, "Resolution for screen and minimap feature layers.")
         flags.DEFINE_integer("step_mul", 8, "Game steps per agent step.")
-        flags.DEFINE_integer("n_models", 1, "Number of models being trained in parallel by PBT.")
-        flags.DEFINE_integer("n_envs_per_model", 16, "Number of environments to run in parallel per model.")
+        flags.DEFINE_integer("n_models", 3, "Number of models being trained in parallel by PBT.")
+        flags.DEFINE_integer("n_envs_per_model", 2, "Number of environments to run in parallel per model.")
         flags.DEFINE_integer("n_steps_per_batch", 8,
                              "Number of steps per batch.")
         flags.DEFINE_integer("all_summary_freq", 50, "Record all summaries every n batch")
@@ -26,7 +26,7 @@ class Config:
         flags.DEFINE_string("model_name", "temp_testing", "Name for checkpoints and tensorboard summaries")
         flags.DEFINE_integer("K_batches", -1,
                              "Number of training batches to run in thousands, use -1 to run forever")
-        flags.DEFINE_integer("K_batches_per_eval", 1,
+        flags.DEFINE_integer("K_batches_per_eval", 10,
                              "Number of training batches to run in thousands before PBT evaluates model")
         flags.DEFINE_string("map_name", "CollectMineralShards", "Name of a map to use.")
         flags.DEFINE_boolean("training", True,
@@ -94,5 +94,5 @@ class Config:
         # other. In that case it could be possible, that when replacing a less memory
         # demanding model with a more demanding one, we could run out of memory. I did not
         # make a better workaround yet.
-        self.tf_config.gpu_options.allow_growth = True
-        self.tf_config.gpu_options.per_process_gpu_memory_fraction = 0.90 / FLAGS.n_models
+        # self.tf_config.gpu_options.allow_growth = True
+        self.tf_config.gpu_options.per_process_gpu_memory_fraction = 0.80 / FLAGS.n_models
